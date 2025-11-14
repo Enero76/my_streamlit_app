30e02be54b149ce6df5ae575c80874cfa7885e5c
Jaime <Jaime@TROMW044877.emea.roche.com>
2025-11-14 16:53:51 +0100

    Removed columns and set to be wide by default

--- app.py
+++ app.py
@@ -50,10 +50,9 @@ df_plants_types.columns = ["Capacity (MW)","Production (MWh)"]
 
 
 # Create the page title
-left_col, middle_col, right_col = st.columns([1,10,1])
-with middle_col:
-    st.set_page_config(layout="wide",page_icon="renewable-icon.png")
-    st.title("Renewable Energy in Switzerland")
+
+st.set_page_config(layout="wide",page_icon="renewable-icon.png")
+st.title("Renewable Energy in Switzerland")
 
 
 
@@ -95,35 +94,34 @@ df_plants['canton_str'] = df_plants['canton'].map(cantons_dict)
 
 df_sum = df_plants.groupby("canton_str", as_index=False)["production"].sum()
 
-with middle_col:
-    st.header("Renewable Energy Production and Cantons")
-    middle_col.write(f"Total production in Switzerland is {round(total_prod_GW,3)} GWh")
+st.header("Renewable Energy Production and Cantons")
+st.write(f"Total production in Switzerland is {round(total_prod_GW,3)} GWh")
 
-    fig = go.Figure(go.Choroplethmap(geojson=swiss_areas, locations=df_sum.canton_str,
-                                    featureidkey='properties.kan_name',
-                                    z=df_sum["production"],
-                                    colorscale="Viridis",
-                                    zmin=0,
-                                    zmax=df_sum["production"].max(),
-                                    marker_opacity=0.5, marker_line_width=0,
-                                    hovertext=df_sum.apply(
-                                        lambda row: f"{row['canton_str']}<br>{row['production']:,.0f} MWh",
-                                        axis=1),
-                                    hoverinfo="text"
-                                    ))
+fig = go.Figure(go.Choroplethmap(geojson=swiss_areas, locations=df_sum.canton_str,
+                                featureidkey='properties.kan_name',
+                                z=df_sum["production"],
+                                colorscale="Viridis",
+                                zmin=0,
+                                zmax=df_sum["production"].max(),
+                                marker_opacity=0.5, marker_line_width=0,
+                                hovertext=df_sum.apply(
+                                    lambda row: f"{row['canton_str']}<br>{row['production']:,.0f} MWh",
+                                    axis=1),
+                                hoverinfo="text"
+                                ))
 
-    fig.update_layout(map_style="carto-positron",
-                    map_zoom=7, map_center = {"lat": 46.79911910358427, "lon": 8.116236707428286},
-                    margin={"r":0,"t":40,"l":0,"b":0}, title={
-                            'text': "Renewable Energy Production (MWh)",
-                            'x': 0.5,
-                            'xanchor': 'center'
-                                }
-                    ,height=700
-                    ,width=1200
-                    )
+fig.update_layout(map_style="carto-positron",
+                map_zoom=7, map_center = {"lat": 46.79911910358427, "lon": 8.116236707428286},
+                margin={"r":0,"t":40,"l":0,"b":0}, title={
+                        'text': "Renewable Energy Production (MWh)",
+                        'x': 0.5,
+                        'xanchor': 'center'
+                            }
+                ,height=700
+                ,width=1200
+                )
 
-    st.plotly_chart(fig, width='content')
+st.plotly_chart(fig, width='content')
 
     #
 
@@ -131,146 +129,145 @@ with middle_col:
 # Renewable Plants in Switzerland
 ##################################
 
-with middle_col:
-    st.write("\n\n")
-    st.write("\n\n")
-    st.write("\n\n")
-    st.header("Renewable Power Plants in Switzerland")
-
-    
-
-    
-    mini_left, mini_middle, mini_right = middle_col.columns([1,1,1])
-    mini_middle.dataframe(df_plants_types.style.format({"Production (MWh)": "{:,.0f}", "Capacity (MWh)": "{:,.f}"}))
+st.write("\n\n")
+st.write("\n\n")
+st.write("\n\n")
+st.header("Renewable Power Plants in Switzerland")
 
 
 
-    fig = px.scatter_map(
-        df_plants,
-        lat="lat",
-        lon="lon",
-        size="electrical_capacity",                 # Tamaño del círculo
-        color="energy_source_level_2",              # for the classification
-        labels={
-                "energy_source_level_2": "Energy Source",
-                "electrical_capacity" : "Installed Capacity (MW)",
-                "energy_source_level_3" : "Plant Type",
-                "production" : "Production (MWh)",
+
+mini_left, mini_middle, mini_right = st.columns([1,1,1])
+mini_middle.dataframe(df_plants_types.style.format({"Production (MWh)": "{:,.0f}", "Capacity (MWh)": "{:,.f}"}))
+
+
+
+fig = px.scatter_map(
+    df_plants,
+    lat="lat",
+    lon="lon",
+    size="electrical_capacity",                 # Tamaño del círculo
+    color="energy_source_level_2",              # for the classification
+    labels={
+            "energy_source_level_2": "Energy Source",
+            "electrical_capacity" : "Installed Capacity (MW)",
+            "energy_source_level_3" : "Plant Type",
+            "production" : "Production (MWh)",
+            },
+    hover_data = {
+                    "energy_source_level_2" : True,
+                    "electrical_capacity" : True,
+                    "energy_source_level_3" : True,
+                    "production" : True,
+                    "lat" : False,
+                    "lon" : False,
                 },
-        hover_data = {
-                        "energy_source_level_2" : True,
-                        "electrical_capacity" : True,
-                        "energy_source_level_3" : True,
-                        "production" : True,
-                        "lat" : False,
-                        "lon" : False,
-                    },
-        hover_name="municipality",      # Texto al pasar el ratón
-        size_max=30,                  # Tamaño máximo de los círculos
-        zoom=7,
-        map_style="carto-positron",
-        center={"lat": 46.8, "lon": 8.2},
-        height=700,
-        width=1200
-    )
+    hover_name="municipality",      # Texto al pasar el ratón
+    size_max=30,                  # Tamaño máximo de los círculos
+    zoom=7,
+    map_style="carto-positron",
+    center={"lat": 46.8, "lon": 8.2},
+    height=700,
+    width=1200
+)
 
-    fig.update_layout(margin={"r":0,"t":40,"l":0,"b":0}, title={
-            'text': "Renewable Power Plants Capacity (MW)",
-            'x': 0.5,
-            'xanchor': 'center'
-        })
+fig.update_layout(margin={"r":0,"t":40,"l":0,"b":0}, title={
+        'text': "Renewable Power Plants Capacity (MW)",
+        'x': 0.5,
+        'xanchor': 'center'
+    })
 
-    st.plotly_chart(fig, width='content')
+st.plotly_chart(fig, width='content')
 
 
 #####################
 # Both Maps Together
 #####################
-with middle_col:
-    st.write("\n\n")
-    st.write("\n\n")
-    st.write("\n\n")
-    st.header("Renewable Power Plants and Cantons Production")
-    df_sum = df_plants.groupby("canton_str", as_index=False)["production"].sum()
 
-    fig = go.Figure(go.Choroplethmap(
-        geojson=swiss_areas,
-        locations=df_sum.canton_str,
-        featureidkey='properties.kan_name',
-        z=df_sum["production"],
-        colorscale="Viridis",
-        zmin=0,
-        zmax=df_sum["production"].max(),
-        marker_opacity=0.5,
-        marker_line_width=0,
-        hovertext=df_sum.apply(
-            lambda row: f"{row['canton_str']}<br>{row['production']:,.0f} MWh",
-            axis=1),
-        hoverinfo="text"
-    ))
+st.write("\n\n")
+st.write("\n\n")
+st.write("\n\n")
+st.header("Renewable Power Plants and Cantons Production")
+df_sum = df_plants.groupby("canton_str", as_index=False)["production"].sum()
 
-    fig.update_layout(
-        map_style="carto-positron",
-        map_zoom=7,
-        map_center={"lat": 46.79911910358427, "lon": 8.116236707428286},
-        margin={"r":0,"t":40,"l":0,"b":0},
-        title={
-            'text': "Renewable Power Plants and Cantons",
-            'x': 0.5,
-            'xanchor': 'center'
-        },
-        height=700
-    )
+fig = go.Figure(go.Choroplethmap(
+    geojson=swiss_areas,
+    locations=df_sum.canton_str,
+    featureidkey='properties.kan_name',
+    z=df_sum["production"],
+    colorscale="Viridis",
+    zmin=0,
+    zmax=df_sum["production"].max(),
+    marker_opacity=0.5,
+    marker_line_width=0,
+    hovertext=df_sum.apply(
+        lambda row: f"{row['canton_str']}<br>{row['production']:,.0f} MWh",
+        axis=1),
+    hoverinfo="text"
+))
 
-    # We add the scatter plot:
-    scatter = px.scatter_map(
-        df_plants,
-        lat="lat",
-        lon="lon",
-        size="electrical_capacity",
-        color="energy_source_level_2",
-        labels={
-            "energy_source_level_2": "Energy Source",
-            "electrical_capacity": "Installed Capacity (MW)",
-            "energy_source_level_3": "Plant Type",
-            "production": "Production (MWh)",
-        },
-        hover_data={
-            "energy_source_level_2": True,
-            "electrical_capacity": True,
-            "energy_source_level_3": True,
-            "production": True,
-            "lat": False,
-            "lon": False,
-        },
-        hover_name="municipality",
-        size_max=30,
-        zoom=7,
-        map_style="carto-positron",
-        center={"lat": 46.8, "lon": 8.2},
-        height=700,
-        width=1200
-    )
+fig.update_layout(
+    map_style="carto-positron",
+    map_zoom=7,
+    map_center={"lat": 46.79911910358427, "lon": 8.116236707428286},
+    margin={"r":0,"t":40,"l":0,"b":0},
+    title={
+        'text': "Renewable Power Plants and Cantons",
+        'x': 0.5,
+        'xanchor': 'center'
+    },
+    height=700
+)
 
-    # Añadimos las trazas de scatter al fig principal
-    for trace in scatter.data:
-        fig.add_trace(trace)
+# We add the scatter plot:
+scatter = px.scatter_map(
+    df_plants,
+    lat="lat",
+    lon="lon",
+    size="electrical_capacity",
+    color="energy_source_level_2",
+    labels={
+        "energy_source_level_2": "Energy Source",
+        "electrical_capacity": "Installed Capacity (MW)",
+        "energy_source_level_3": "Plant Type",
+        "production": "Production (MWh)",
+    },
+    hover_data={
+        "energy_source_level_2": True,
+        "electrical_capacity": True,
+        "energy_source_level_3": True,
+        "production": True,
+        "lat": False,
+        "lon": False,
+    },
+    hover_name="municipality",
+    size_max=30,
+    zoom=7,
+    map_style="carto-positron",
+    center={"lat": 46.8, "lon": 8.2},
+    height=700,
+    width=1200
+)
 
-    fig.update_layout(
-        legend=dict(
-            title="Energy Source",   # título de la leyenda
-            x=0.02,                  # posición horizontal (0 = izquierda, 1 = derecha)
-            y=0.98,                  # posición vertical (0 = abajo, 1 = arriba)
-            bgcolor="rgba(255,255,255,0.7)",  # fondo semitransparente para que no tape el mapa
-            bordercolor="gray",
-            borderwidth=1,
-        ),
-        coloraxis_colorbar=dict(
-            title="Production (MWh)",
-            x=1.02                    # mueve la barra de colores un poco a la derecha
-        ),
-        height=700,
-        width=1200
-    )
+# Añadimos las trazas de scatter al fig principal
+for trace in scatter.data:
+    fig.add_trace(trace)
 
-    st.plotly_chart(fig, width='content')
+fig.update_layout(
+    legend=dict(
+        title="Energy Source",   # título de la leyenda
+        x=0.02,                  # posición horizontal (0 = izquierda, 1 = derecha)
+        y=0.98,                  # posición vertical (0 = abajo, 1 = arriba)
+        bgcolor="rgba(255,255,255,0.7)",  # fondo semitransparente para que no tape el mapa
+        bordercolor="gray",
+        borderwidth=1,
+    ),
+    coloraxis_colorbar=dict(
+        title="Production (MWh)",
+        x=1.02                    # mueve la barra de colores un poco a la derecha
+    ),
+    height=700,
+    width=1200
+)
+
+st.plotly_chart(fig, width='content')
